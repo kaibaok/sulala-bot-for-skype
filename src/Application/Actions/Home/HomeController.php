30a04f846cc5d5dbd66325c348bc319fd064d3ae
vhtam <vo.hoai.tam@quantic.com.vn>
2020-12-09 17:34:06 +0700

    test new bot

--- src/Application/Actions/Home/HomeController.php
+++ src/Application/Actions/Home/HomeController.php
@@ -16,12 +16,16 @@ class HomeController
      */
     protected $logger;
 
-    private $token = "xoxb-1563412321988-1560979488099-NXLqYMQyrFG8CpPqi7y4Yfw4";
+    private $token = "xoxb-1563412321988-1560979488099-lBNzrDoaArlXwnJZd6Zrvv9w";
+    
+    private $hook = "https://hooks.slack.com/services/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";
     
     private $endPoint = "https://slack.com/api";
     
+    private $channelID = "C01FYEYDCLF";
+    
     private $actions = [
-        "postMessage" => "",
+        "postMessage" => "chat.postMessage",
         "conversations" => "conversations.list"
         
     ];
@@ -36,15 +40,16 @@ class HomeController
 
     public function __invoke(Request $request, Response $response, $agrs)
     {
-        $listChannel = $this->getConversations();
-        
-        $this->logger->info(var_export($listChannel,true));
-        // $json = json_encode(['asdasd'=> 'asdasd'], JSON_PRETTY_PRINT);
-        $response->getBody()->write($listChannel);
+        // $result = $this->getConversations();
+        $result = $this->postMessage("Hellloooooo");
+        // $this->logger->info(var_export($listChannel,true));
+        // // $json = json_encode(['asdasd'=> 'asdasd'], JSON_PRETTY_PRINT);
+        $response->getBody()->write($result);
         return $response->withAddedHeader('Content-Type', 'application/json');
     }
     
-    protected function getConversations() {
+    protected function getConversations() 
+    {
         $client = new Client([
             'headers' => [
                 'Authorization' => "Bearer ".$this->token,
@@ -55,6 +60,29 @@ class HomeController
         $response = $client->request('GET', 
             $this->endPoint."/".$this->actions['conversations']            
         );
+        return $response->getBody()->getContents();
+    }
+    
+    protected function postMessage($text) 
+    {
+         $client = new Client([
+            'headers' => [
+                'Authorization' => "Bearer ".$this->token,
+                 'Accept'     => 'application/x-www-form-urlencoded',
+            ],
+            'verify' => false
+        ]);
+        
+        $response = $client->request('POST', 
+            $this->endPoint."/".$this->actions['postMessage'],
+            [
+                \GuzzleHttp\RequestOptions::JSON => ["channel" => $this->channelID,
+                "text" => $text
+            ]   
+                
+            ]
+        );
+        
         return $response->getBody()->getContents();
     }
 }
