f03683703386cce1d6efb2d3193e9257456faf76
Kyra0590 <kyra0590@gmail.com>
2020-12-09 20:47:37 +0700

    test

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 require("vendor/autoload.php");
 
-$token = "xoxb-1563412321988-1558340508773-UrWg4WVdSnWK6s52a30Qfd1A";
+$token = "xoxb-1563412321988-1558340508773-Dlk56U4H3TWjoOZssat9CMGv";
 
 return json_encode(["text" => "hello"]);
